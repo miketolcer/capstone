792a204f7dd38344df1f8b7fe0b5738179d7cf7a
miketolcer <miketolcer@yahoo.com>
2021-11-29 21:49:30 -0500

    lot more changes

--- client/clientjs.js
+++ client/clientjs.js
@@ -5,3 +5,25 @@ document.getElementById("pillButton").onclick = function () {
             alert(data);
         })
 }
+
+document.getElementById('subscribe').addEventListener("submit", (e) => {
+    e.preventDefault()
+    const emailInput = document.getElementById('emailInput')
+    const formBody = {
+    Email: emailInput.value
+    }
+
+    axios.post("http://localhost:4500/api/subscribe", formBody)
+    .then((response) => {
+        const emailArry = response.data
+        console.log(emailArry)
+        alert('Your subscribed')
+    })
+})
+document.getElementById('nextFight').onclick = function () {
+    axios.get("http://localhost:4500/api/nextFight")
+        .then(function (response) {
+            const data = response.data;
+            alert(data)
+        })
+}
--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.get("/api/pillButton", (req,res) => {
     "Khabib says: I care about my legacy.",
     "Khabib says: When you have a hard life, a tough life, success becomes very easy.",
     "Khabib says: All my life I train hard, like since I beginning walking.",
-    "You have to be ready for everybody. And that’s why I keep going. You have to learn everyday something, everyday you have to learn.",
-    "If you have a goal. If you want to do something. You have to do hard work. If you work easy, somebody other guy is coming and will take this. You have to work hard.",
-    "I always tell kids to put education first. Because if you have muscles but no brains, then you’ll have problems.",
-    "Win or lose, I believe in giving my best. And that is what I always do.",
-    "When you truly fight for money, and you truly fight for legacy, its two different mentalities. I am here for legacy."
+    "You have to be ready for everybody. And that’s why I keep going. You have to learn everyday something, everyday you have to learn.     -Khabib Nurmagomedov",
+    "If you have a goal. If you want to do something. You have to do hard work. If you work easy, somebody other guy is coming and will take this. You have to work hard.   -Khabib Nurmagomedov",
+    "I always tell kids to put education first. Because if you have muscles but no brains, then you’ll have problems.    -Khabib Nurmagomedov ",
+    "Win or lose, I believe in giving my best. And that is what I always do.   -Khabib Nurmagomedov",
+    "When you truly fight for money, and you truly fight for legacy, its two different mentalities. I am here for legacy.   -Khabib Nurmagomedov"
 
     
     ];
@@ -26,8 +26,17 @@ app.get("/api/pillButton", (req,res) => {
      res.status(200).send(randomQuote);
 })
 
-
-
+let emailArry = []
+app.post("/api/subscribe", (req,res) => {
+    const {Email} = req.body
+    emailArry.push(req.body)
+    console.log(req.body)
+    res.sendStatus(200)
+})
+app.get("/api/nextFight", (req,res) => {
+    const fights = ["December 18, 2021 Lewis vs. Daukaus-- at the UFC APEX in Las Vegas on ESPN+ at 7:00pm "];
+    res.status(200).send(fights)
+})
 
 
 
